85d548cf006d0e558bfc2fdfc1348688e8d60c37
Agata Liberska <liberska@gmail.com>
2020-11-10 15:24:10 +0000

    main score keeping mechanism, style: styling added using Bulma

--- app.js
+++ app.js
@@ -1,27 +1,59 @@
 //player one
-const playerOnePoint = document.querySelector("#playerOnePoint");
+const playerOneButton = document.querySelector("#playerOnePoint");
 const playerOneScore = document.querySelector("#playerOneScore");
 let score1 = 0;
 //player two
-const playerTwoPoint = document.querySelector("#playerTwoPoint");
+const playerTwoButton = document.querySelector("#playerTwoPoint");
 const playerTwoScore = document.querySelector("#playerTwoScore");
 let score2 = 0;
 //reset button
-const reset = document.querySelector('#reset');
+const resetButton = document.querySelector('#reset');
+//winning score
+const winningScoreSelect = document.querySelector('#targetScore');
+let winningScore = 3;
+let isGameOver = false;
 
-playerOnePoint.addEventListener('click', function() {
-    score1 ++;
-    playerOneScore.innerHTML = score1;
-})
+playerOneButton.addEventListener('click', function() {
+    if (!isGameOver) {
+        score1 ++;
+        if (score1 === winningScore) {
+            isGameOver = true;
+            playerOneScore.classList.add('has-text-success');
+            playerTwoScore.classList.add('has-text-danger');
+        }
+        playerOneScore.textContent = score1;
+    }
 
-playerTwoPoint.addEventListener('click', function() {
-    score2 ++;
-    playerTwoScore.innerHTML = score2;
-})
 
-reset.addEventListener('click', function() {
+});
+
+playerTwoButton.addEventListener('click', function() {
+    if (!isGameOver) {
+        score2 ++;
+        if (score2 === winningScore) {
+            isGameOver = true;
+            playerTwoScore.classList.add('has-text-success');
+            playerOneScore.classList.add('has-text-danger');
+        }
+        playerTwoScore.textContent = score2;
+    }
+
+});
+
+
+winningScoreSelect.addEventListener('change', function() {
+    winningScore = parseInt(this.value);
+    reset();
+});
+
+resetButton.addEventListener('click', reset)
+
+function reset() {
+    isGameOver = false;
     score1 =0;
-    playerOneScore.innerHTML = score1;
     score2 =0;
-    playerTwoScore.innerHTML = score2;
-})
+    playerOneScore.textContent = score1;
+    playerTwoScore.textContent = score2;
+    playerOneScore.classList.remove('has-text-success', 'has-text-danger');
+    playerTwoScore.classList.remove('has-text-success', 'has-text-danger');
+}
